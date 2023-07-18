cef3544c7413e0ab28117b5159c8923f4718244a
Jarek Frankowski <jfrankowski@objectivity.co.uk>
2023-07-19 00:32:03 +0200

    refactor search results

--- src/components/search-results.tsx
+++ src/components/search-results.tsx
@@ -29,43 +29,55 @@ export default function SearchResults() {
   return (
     <div>
       <SearchForm onSearch={handleSearch} />
-      {searchResults?.length === 0 && (
-        <div className="border border-gray-700 rounded-md p-4">
-          <h3 className="text-lg font-medium py-5">No results found</h3>
-        </div>
-      )}
-      <div className="md:grid grid-cols-2 gap-4 flex flex-col py-10">
-        {searchResults &&
-          searchResults.map((result) => (
-            <div
-              key={result.id}
-              className="border border-gray-700 rounded-md p-4 hover:bg-gray-900"
-            >
-              <Link href={result.url}>
-                <Shimmer
-                  src={result.image}
-                  alt={result.name}
-                  width={200}
-                  height={200}
-                  className="mx-auto"
-                />
-              </Link>
-              <h3 className="text-lg font-medium py-5">{result.name}</h3>
-              <p className="pb-5">
-                <span className="font-bold text-lg">{result.score}</span>
-                {result.highlights.length > 0 && (
-                  <span
-                    className="line-clamp-2"
-                    dangerouslySetInnerHTML={{ __html: result.highlights[0] }}
-                  ></span>
-                )}
-              </p>
-              <Link href={result.url} className="underline">
-                View Details
-              </Link>
-            </div>
-          ))}
-      </div>
+      <RenderSearchResults searchResults={searchResults} />
     </div>
   );
 }
+
+const RenderSearchResults = ({
+  searchResults,
+}: {
+  searchResults?: SearchResult[];
+}) => {
+  if (searchResults?.length === 0) {
+    return (
+      <div className="border border-gray-700 rounded-md p-4">
+        <h3 className="text-lg font-medium py-5">No results found</h3>
+      </div>
+    );
+  }
+  return (
+    <div className="md:grid grid-cols-2 gap-4 flex flex-col py-10">
+      {searchResults &&
+        searchResults.map((result) => (
+          <div
+            key={result.id}
+            className="border border-gray-700 rounded-md p-4 hover:bg-gray-900"
+          >
+            <Link href={result.url}>
+              <Shimmer
+                src={result.image}
+                alt={result.name}
+                width={200}
+                height={200}
+                className="mx-auto"
+              />
+            </Link>
+            <h3 className="text-lg font-medium py-5">{result.name}</h3>
+            <p className="pb-5">
+              <span className="font-bold text-lg">{result.score}</span>
+              {result.highlights.length > 0 && (
+                <span
+                  className="line-clamp-2"
+                  dangerouslySetInnerHTML={{ __html: result.highlights[0] }}
+                ></span>
+              )}
+            </p>
+            <Link href={result.url} className="underline">
+              View Details
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
